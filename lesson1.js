6d3af62ef83d2cc8c9393950183bb4780960f72e
ivnikonGB <ivnikon@yandex.ru>
2020-02-05 22:24:19 +0300

    Lesson 1 done

---
+++ lesson1.js
@@ -0,0 +1,17 @@
+// Первый пункт ДЗ
+var tC, tF;
+
+tC = parseInt(prompt("Введите температуру в градусах Цельсия:"));
+tF = (9 / 5) * tC + 32;
+alert("Температура в градусах Фаренгейта: " + tF);
+
+// Второй пункт ДЗ
+var admin, name;
+
+name = "Василий";
+admin = name;
+document.write(admin);
+
+// Третий пункт ДЗ
+var a = 1000 + "108";
+console.log(a);
