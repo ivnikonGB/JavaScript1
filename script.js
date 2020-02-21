38a900f3dcefd1f587a36eb518c85df11d77d66c
ivnikonGB <ivnikon@yandex.ru>
2020-02-21 15:32:06 +0300

    lesson done

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function createChessBoard() {
     var board = document.createElement("table");
-    var tr, td;
+    var tr, td, button;
     var letters = ["", "A", "B", "C", "D", "E", "F", "G", "H"];
     document.body.append(board);
     board.border = 1;
@@ -14,11 +14,18 @@ function createChessBoard() {
             td.style.height = 50;
             if(i != 0 && i != 9 && j != 0 && j != 9) {
                 if(i % 2 == 0) {
-                    if(j % 2 == 0) td.style.backgroundColor = 0xffffff
-                    else td.style.backgroundColor = 0x000000;
+                    if(j % 2 == 0){
+                        td.style.backgroundColor = "#cccccc";
+                    } else {
+                        td.style.backgroundColor = "#333333";
+                    }
                 } else {
-                    if(j % 2 == 0) td.style.backgroundColor = 0x000000
-                    else td.style.backgroundColor = 0xffffff;
+                    if(j % 2 == 0) {
+                        td.style.backgroundColor = "#333333";
+                    } 
+                    else {
+                        td.style.backgroundColor = "#cccccc";
+                    }
                 }
             } else {
                 td.style.borderStyle = "none";
@@ -38,7 +45,55 @@ function createChessBoard() {
         }
         board.appendChild(tr);
     }
-    
+    button = document.createElement("button");
+    button.onclick = putLetterFigures;
+    button.innerText = "Добавить фигуры на доску";
+    button.style.margin = "50px 180px";
+    document.body.append(button);
 }
+
+function putFigures(figures) {
+    var tr = document.querySelectorAll("tr");
+    var figuresBlack = tr[1].querySelectorAll("td");
+    var pawnsBlack = tr[2].querySelectorAll("td");
+    var figuresWhite = tr[8].querySelectorAll("td");
+    var pawnsWhite = tr[7].querySelectorAll("td");
+    
+    for(var i=0;i<8;i++) {
+        figuresBlack[i+1].innerHTML = figures[i];
+        figuresBlack[i+1].style.color = "#000000";
+        figuresBlack[i+1].style.fontSize = "30px";
+        pawnsBlack[i+1].innerHTML = figures[8];
+        pawnsBlack[i+1].style.color = "#000000";
+        pawnsBlack[i+1].style.fontSize = "30px";
+        figuresWhite[i+1].innerHTML = figures[i];
+        figuresWhite[i+1].style.color = "#ffffff";
+        figuresWhite[i+1].style.fontSize = "30px";
+        pawnsWhite[i+1].innerHTML = figures[8];
+        pawnsWhite[i+1].style.color = "#ffffff";
+        pawnsWhite[i+1].style.fontSize = "30px";
+    }
+    return;
+}
+
+function putLetterFigures() {
+    var letterFigures = ["R", "N", "B", "Q", "K", "B", "N", "R", "P"];
+    var button = document.querySelector("button");
+    button.innerText = "Поменять буквы на изображения";
+    button.onclick = putRealFigures;
+    putFigures(letterFigures);
+    return;
+}
+
+function putRealFigures() {
+    var figures = ["&#9814;", "&#9816;", "&#9815;", "&#9813;", "&#9812;", "&#9815;", "&#9816;", "&#9814;", "&#9817;"];
+    var button = document.querySelector("button");
+    button.onclick = putLetterFigures;
+    button.innerText = "Добавить фигуры на доску";
+    putFigures(figures);
+    return;
+}
+
+
 
 createChessBoard();
